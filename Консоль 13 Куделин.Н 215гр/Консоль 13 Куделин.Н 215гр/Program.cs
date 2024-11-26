cce0da144703abb0be7b308c41d56851ff37f66d
KudelinNikita <Rokotanskiy8@gmail.com>
2024-11-26 14:12:59 +0200

    Add files via upload

---
+++ Консоль 13 Куделин.Н 215гр/Консоль 13 Куделин.Н 215гр/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Консоль_13_Куделин.Н_215гр
+{
+    /// <summary>
+    /// Найти минимальную величину из двух целых переменных a, b, 
+    /// используя вспомогательные методы (ввода аргументов, нахождения минимальной величины, вывода результата).
+    /// </summary>
+    class Program
+    {
+        static int InputValue(string varName)
+        {
+
+            Console.Write($"Введите значение переменной {varName}: ");
+            return int.Parse(Console.ReadLine());
+        }
+        static int FindMinimum(int x, int y)
+        {
+            return x < y ? x : y;
+        }
+        static void PrintResult(int result)
+        {
+            Console.WriteLine($"Минимальная величина между введенными числами: {result}");
+        }
+        static void Main()
+        {
+
+
+            int x = InputValue("a");
+            int y = InputValue("b");
+            int minimum = FindMinimum(x,y);
+            PrintResult(minimum);
+            Console.Read();
+        }
+    }
+}
